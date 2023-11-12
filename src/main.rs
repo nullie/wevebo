a561d297e165ae6bb561b26b5ea1446e81ccca51
Ilia Novoselov <i.novoselov@eclecticiq.com>
2023-11-12 21:35:42 +0100

    Added social

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use irc::client::prelude::*;
+use std::collections::{HashSet, VecDeque};
+
 use futures::prelude::*;
+use irc::{client::prelude::*, error};
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
@@ -16,9 +18,55 @@ async fn main() -> Result<(), failure::Error> {
 
     let mut stream = client.stream()?;
 
+    let mut social = Social::new(&client);
+
     while let Some(message) = stream.next().await.transpose()? {
-        print!("{}", message);
+        println!("{:?}", message);
+        social.handle_privmsg(message)?
     }
 
     Ok(())
+}
+
+struct Social<'a> {
+    client: &'a Client,
+    previous_messages: VecDeque<(String, String)>,
+}
+
+impl<'a> Social<'a> {
+    fn new(client: &'a Client) -> Self {
+        Social {
+            client,
+            previous_messages: VecDeque::new(),
+        }
+    }
+    fn handle_privmsg(&mut self, message: Message) -> error::Result<()> {
+        if let Some(Prefix::Nickname(nickname, _, _)) = message.prefix {
+            if let Command::PRIVMSG(channel, text) = message.command {
+                if self.previous_messages.len() == 3 {
+                    self.previous_messages.pop_front();
+                }
+                self.previous_messages.push_back((nickname, text.clone()));
+                assert!(self.previous_messages.len() <= 3);
+                if self.previous_messages.len() == 3 {
+                    let nicknames: HashSet<_> = self
+                        .previous_messages
+                        .iter()
+                        .map(|(nickname, _)| nickname)
+                        .collect();
+                    let texts: HashSet<_> = self
+                        .previous_messages
+                        .iter()
+                        .map(|(_, text)| text.trim().to_lowercase())
+                        .collect();
+                    if nicknames.len() == 3 && texts.len() == 1 {
+                        self.client.send_privmsg(channel, text)?;
+                        self.previous_messages.clear();
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
